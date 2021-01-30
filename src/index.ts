a842135304cdc760da6ff15bbe84427d544b0a9f
清川　裕太郎 <yuu7353@gmail.com>
2021-01-30 18:53:37 +0900

    Add basic type

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,10 @@ import World from './world'
 const root: HTMLElement | null = document.getElementById('root')
 const world = new World('Hello World!!!')
 world.sayHello(root)
+
+//基本の型定義
+//import { anySample, notExist, primitiveSample, unknownSample } from './basic'
+//anySample()
+//notExist()
+//primitiveSample()
+//unknownSample()
